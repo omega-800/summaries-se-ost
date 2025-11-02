79307d3ca8415553e5c0c47cec58277f9c6cbf48
omega <georgiy.shevoroshkin@ost.ch>
2025-11-02 12:20:35 +0100

    forgor

--- flake.nix
+++ flake.nix
@@ -37,6 +37,7 @@
       typixPkgs =
         pkgs:
         let
+          typixLib = typix.lib.${pkgs.system};
           fs = pkgs.lib.fileset;
           sources = pkgs.lib.pipe ./. [
             (fs.fileFilter (f: f.name == "doc.typ"))
@@ -46,12 +47,15 @@
             (map (n: builtins.elemAt n 0))
           ];
           watchScriptsPerDoc = map (
-            typstSource: typixLib.watchTypstProject (commonArgs // { 
-              inherit typstSource; 
-              typstOutput = (pkgs.lib.removeSuffix ".typ" typstSource) + ".pdf";
-            })
+            typstSource:
+            typixLib.watchTypstProject (
+              commonArgs
+              // {
+                inherit typstSource;
+                typstOutput = (pkgs.lib.removeSuffix ".typ" typstSource) + ".pdf";
+              }
+            )
           ) sources;
-          typixLib = typix.lib.${pkgs.system};
           commonArgs = {
             typstOpts.root = ".";
             typstSource = "lib.typ";
@@ -74,12 +78,9 @@
           build-script = typixLib.buildTypstProjectLocal (commonArgs // extraArgs);
           watch-script = typixLib.watchTypstProject commonArgs;
           watch-all = pkgs.writeShellApplication {
-            excludeShellChecks = [ "SC2046" ];
-            text = ''
-              (trap 'kill 0' SIGINT; ${
-                pkgs.lib.concatMapStringsSep " & " (s: "${s}/bin/typst-watch") watchScriptsPerDoc
-              })
-            '';
+            text = "(trap 'kill 0' SIGINT; ${
+              pkgs.lib.concatMapStringsSep " & " (s: "${s}/bin/typst-watch") watchScriptsPerDoc
+            })";
             name = "typst-watch-all";
           };
         };
